10c37f7c4b555f08aa24947de9e4af96111d6a69
jenny83318 <jenny83318@gmail.com>
2023-04-14 10:15:43 +0800

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/jol-ui'),
+      dir: require('path').join(__dirname, './coverage/jolui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'JOL_UI'`, () => {
+  it(`should have as title 'jolui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('JOL_UI');
+    expect(app.title).toEqual('jolui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('JOL_UI app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jolui app is running!');
   });
 });
