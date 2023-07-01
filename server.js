2c17b4231189fad230ad392569762d161acb2206
jenny83318 <jenny83318@gmail.com>
2023-07-01 16:14:06 +0800

    update angular

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist'));
+// app.get('/*', function(req, res){
+// res.sendFile(path.join(__dirname + '/dist/index.html'));});
+// app.listen(process.env.POST || 8080);
+
 const express = require('express');
-const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
-app.use(express.static(_dirname + '/dist/jolui'));
-app.get('/*', function(req, res){
-res.sendFile(path.join(_dirname + '/dist/jolui/index.html'));});
-app.listen(process.env.POST || 8080);
+
+app.use(express.static(__dirname + '/dist'));
+
+// 设置代理
+app.use('/jolserver', createProxyMiddleware({
+  target: 'http://139.162.97.43:8080',
+  changeOrigin: true,
+  secure: false,
+  pathRewrite: {
+    '^/jolserver/api': '/jolserver/api'
+  }
+}));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+
+
+
+
