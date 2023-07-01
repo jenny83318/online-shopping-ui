b682bb15cb8e0ce5c768ddd4e8bf0d62e0fc4f3a
jenny83318 <jenny83318@gmail.com>
2023-07-01 13:23:53 +0800

    update enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  JOLSERVER: "/jolserver/api/json",
+  STRIPE:"/jolserver/api/payment",
+  STRIPEKEY: 'pk_test_51NIikVB9Nt3grzHaDL44jSn6ycDrs74JlW1iVO7adgmHVJL8dHKSDdwv9athWLV5vya2uOXAP53td2q2eXlClsJL002FlT3ofK',
 };
