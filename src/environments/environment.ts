a4aec197d78a13d82eb9a7b98849332200e992a6
jenny83318 <jenny83318@gmail.com>
2023-10-19 14:30:03 +0800

    update domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
   STRIPE:"/jolserver/api/payment",
   Render_Succ:"https://jol-boutique.onrender.com/orderlist",
   Render_Fail:"https://jol-boutique.onrender.com",
-  SUCCESS_URL:"https://forest-bhfq.com/orderlist",
-  FAIL_URL:"https://forest-bhfq.com",
+  SUCCESS_URL:"https://ocean-hcz.com/orderlist",
+  FAIL_URL:"https://ocean-hcz.com/",
   STRIPEKEY: 'pk_test_51NIikVB9Nt3grzHaDL44jSn6ycDrs74JlW1iVO7adgmHVJL8dHKSDdwv9athWLV5vya2uOXAP53td2q2eXlClsJL002FlT3ofK',
 };
 
