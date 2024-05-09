35c71fe2854529c961a047e241c1fc24dceefe8f
jenny83318 <jenny83318@gmail.com>
2024-05-09 21:32:45 +0800

    update server.js

--- server.js
+++ server.js
@@ -8,16 +8,20 @@
 
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const cors =require('cors');
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
-
 app.use(express.static(__dirname + '/dist'));
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+    credentials: true,
+    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+  })
+);
 // 设置代理
 app.use('/jolserver', createProxyMiddleware({
   target: 'http://jol-server.onrender.com',
