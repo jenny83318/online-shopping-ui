497e596e8562dc0f1f936ca944b1c57ac036403d
jenny83318 <jenny83318@gmail.com>
2024-05-09 20:44:13 +0800

    update server.js

--- server.js
+++ server.js
@@ -8,8 +8,11 @@
 
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors =require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static(__dirname + '/dist'));
 
