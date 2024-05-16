1bee32c298389f8e08494d714c062043e4d9539b
jenny83318 <jenny83318@gmail.com>
2024-05-16 14:45:37 +0800

    Revert "update server.js"
    This reverts commit 4bc8e9a2260cf67ad4a9bac67f6be1a962100e10.

--- server.js
+++ server.js
@@ -14,34 +14,20 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-// 添加 CORS 中間件
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://jol-boutique.onrender.com" // 替換為你的前端應用程式的域名
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+    credentials: true,
+    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+  })
+);
 // 设置代理
 app.use('/jolserver', createProxyMiddleware({
   target: 'http://jol-server.onrender.com',
   changeOrigin: true,
   secure: false,
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: { 'Access-Control-Allow-Origin': '*' }, // 添加這一行
   pathRewrite: {
     '^/jolserver/api': '/jolserver/api'
   }
@@ -55,3 +41,9 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+
+
+
