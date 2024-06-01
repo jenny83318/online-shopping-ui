c22abce7991e4b88294949457f78d1d030d6c621
jenny83318 <jenny83318@gmail.com>
2024-06-02 01:05:23 +0800

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
   Render_Fail:"https://jol-boutique.onrender.com",
   SUCCESS_URL:"https://jol-boutique.imcarpediem.com/orderlist",
   FAIL_URL:"https://jol-boutique.imcarpediem.com/",
-  IMG_URL:"https://github.com/jenny83318/online-shopping-ui/blob/master/src/assets",
+  IMG_URL:"https://github.com/jenny83318/online-shopping-ui/blob/prod/src/assets",
   STRIPEKEY: 'pk_test_51NIikVB9Nt3grzHaDL44jSn6ycDrs74JlW1iVO7adgmHVJL8dHKSDdwv9athWLV5vya2uOXAP53td2q2eXlClsJL002FlT3ofK',
 };
